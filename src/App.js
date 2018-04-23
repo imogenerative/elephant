d0515d21726dec3f33fefb70769cc3cd87cacf11
Imogen Fulton <imogen@dfdt.me>
2018-04-23 12:27:38 +1000

    basic structure of the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const JSON = 'https://www.reddit.com/r/javascript/top/.json';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(JSON)
+            .then(response => response.json())
+            .then(data => this.setState({ posts: data.data.children }));
+    }
+
+    render() {
+        const { posts } = this.state;
+
+        return (
+                <div>
+                {posts.map(post =>
+                           <div id="post">
+                               <div id="title">{ post.data.title }</div> by <span id="author">{ post.data.author }</span>
+                               <div id="url"><a href={ post.data.url }>{ post.data.url }</a></div>
+                           </div>
+                          )}
+                </div>
+        );
+    }
 }
 
 export default App;
